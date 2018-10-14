4bc4ab93e24b0f4f0145e60ac64edf99b6a02af9
Diane Chen <dianedc@purpleweb.com>
2018-10-13 21:00:21 -0700

    OOPS. travis-ci builds not working with 3.7 for me

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     description="Flatten a 2D list of lists",
     entry_points={
